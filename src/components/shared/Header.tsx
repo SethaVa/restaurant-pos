da98413b53181f60485c472488b64c6efc5442e4
Setha Va <vasetha1@gmail.com>
2025-11-30 14:49:12 +0700

    Project header

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,36 +1,35 @@
-import React from 'react';
 import logo from "../../assets/react.svg";
 import { FaBell, FaSearch, FaUserCircle } from 'react-icons/fa';
 
 const Header = () => {
   return (
-    <header className='flex justify-between items-center py-4 px-8 bg-[#1a1a1a]'>
+    <header className='flex justify-between items-center py-4 px-8 bg-[var(--color-background)]'>
       {/* LOGO */}
       <div className='flex items-center gap-2'>
         <img src={logo} className='h-8 w-8' alt='restro logo' />
-        <h1 className='text-lg font-semibold text-[#f5f5f5]'>Restro</h1>
+        <h1 className='text-lg font-semibold text-[var(--color-primary)]'>Restro</h1>
       </div>
 
       {/* SEARCH */}
-      <div className='flex items-center gap-4 bg-[#1f1f1f] rounded-[15px] px-5 py-2 w-[500px]'>
-        <FaSearch className='text-[#f5f5f5]'/>
+      <div className='flex items-center gap-4 bg-[var(--color-background)] rounded-[15px] px-5 py-2 w-[500px]'>
+        <FaSearch className='text-[var(--color-primary)]'/>
         <input 
             type='text'
             placeholder='Search'
-            className='bg-[#1f1f1f] text-[#f5f5f5] outline-none w-full'
+            className='bg-[var(--color-background)] text-[var(--color-primary)] outline-none w-full'
         />
       </div>
 
       {/* LOGGED USER DETAILS */}
       <div className='flex items-center gap-4'>
-        <div className='bg-[#1f1f1f] rounded-[15px] p-3 cursor-pointer'>
-            <FaBell className='text-[#f5f5f5] text-2xl'/>
+        <div className='bg-[var(--color-background)] rounded-[15px] p-3 cursor-pointer'>
+            <FaBell className='text-[var(--color-primary)] text-2xl'/>
         </div>
         <div className='flex items-center gap-3 cursor-pointer'>
-            <FaUserCircle className='text-[#f5f5f5] text-2xl' />
-            <div>
-                <h1>Amrit Raj</h1>
-                <p>Admin</p>
+            <FaUserCircle className='text-[var(--color-primary)] text-4xl' />
+            <div className='text-md items-start'>
+                <h1 className="text-md text-[var(--color-primary)] font-semibold">Amrit Raj</h1>
+                <p className="text-xs text-[#ababab] font-medium">Admin</p>
             </div>
         </div>
       </div>
