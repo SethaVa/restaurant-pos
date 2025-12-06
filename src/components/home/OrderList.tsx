12c7e5f04c575ca2921fd54f7aa780dd532dcbeb
Setha Va <vasetha1@gmail.com>
2025-12-06 23:18:52 +0700

    Order page UI design

--- src/components/home/OrderList.tsx
+++ src/components/home/OrderList.tsx
@@ -1,35 +1,39 @@
-import React from 'react'
-import { FaCheckDouble, FaCircle } from 'react-icons/fa'
+import React from "react";
+import { FaCheckDouble, FaCircle } from "react-icons/fa";
 
 const OrderList = () => {
   return (
-    <div className='flex items-center gap-5 mb-3'>
-      <button className='bg-[var(--color-currency)] p-3 text-xl font-bold rounded-lg'>AM</button>
-      <div className='flex items-center justify-between w-[100%]'>
-        <div className='flex flex-col items-start gap-1'>
-            <h1 className='text-[var(--color-primary)] text-lg font-semibold tracking-wide'>Amrit Raj</h1>
-            <p className='text-[var(--color-secondary)] text-sm'>8 Items</p>
+    <div className="flex items-center gap-5 mb-3">
+      <button className="bg-[var(--color-currency)] p-3 text-xl font-bold rounded-lg">
+        AM
+      </button>
+      <div className="flex items-center justify-between w-[100%]">
+        <div className="flex flex-col items-start gap-1">
+          <h1 className="text-[var(--color-primary)] text-lg font-semibold tracking-wide">
+            Amrit Raj
+          </h1>
+          <p className="text-[var(--color-secondary)] text-sm">8 Items</p>
         </div>
 
-        <div className=''>
-            <h1 className='text-[var(--color-currency)] font-semibold border border-[var(--color-currency)] rounded-lg p-1'>
-                Table No: 3
-            </h1>
+        <div className="">
+          <h1 className="text-[var(--color-currency)] font-semibold border border-[var(--color-currency)] rounded-lg p-1">
+            Table No: 3
+          </h1>
         </div>
 
-        <div className='flex flex-col items-start gap-2'>
-            <p className='text-green-600 px-4'>
-                <FaCheckDouble className='inline mr-2'/>
-                Ready
-            </p>
-            <p className='text-[var(--color-secondary)] text-sm'>
-                <FaCircle className='inline mr-2 text-green-600'/>
-                Ready to serve
-            </p>
+        <div className="flex flex-col items-end gap-2">
+          <p className="text-green-600">
+            <FaCheckDouble className="inline mr-2" />
+            Ready
+          </p>
+          <p className="text-[var(--color-secondary)] text-sm">
+            <FaCircle className="inline mr-2 text-green-600" />
+            Ready to serve
+          </p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default OrderList
+export default OrderList;
--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -1,11 +1,71 @@
-import React from 'react'
+import React, { useState } from "react";
+import BottomNav from "../components/shared/BottomNav";
+import OrderCard from "../components/orders/OrderCard";
+import BackButton from "../components/shared/BackButton";
 
 const Orders = () => {
+  const [status, setStatus] = useState("all");
   return (
-    <div>
-      Orders
-    </div>
-  )
-}
+    <section className="bg-[var(--color-background)] h-[calc(100vh-8rem)] overflow-auto scrollbar-hide">
+      <div className="flex items-center justify-between px-10 py-4">
+        <div className="flex items-center gap-4">
+          <BackButton />
+          <h1 className="text-[var(--color-primary)] text-2xl font-bold tracking-wide">
+            Orders
+          </h1>
+        </div>
+        <div className="flex items-center justify-around gap-4">
+          <button
+            onClick={() => setStatus("all")}
+            className={`text-[var(--color-secondary)] text-lg rounded-lg px-5 py-2 font-semibold ${
+              status == "all" && "bg-[var(--color-dark-gray)]"
+            }`}
+          >
+            All
+          </button>
+          <button
+            onClick={() => setStatus("in_progress")}
+            className={`text-[var(--color-secondary)] text-lg rounded-lg px-5 py-2 font-semibold ${
+              status == "in_progress" && "bg-[var(--color-dark-gray)]"
+            }`}
+          >
+            In Progress
+          </button>
+          <button
+            onClick={() => setStatus("ready")}
+            className={`text-[var(--color-secondary)] text-lg rounded-lg px-5 py-2 font-semibold ${
+              status == "ready" && "bg-[var(--color-dark-gray)]"
+            }`}
+          >
+            Ready
+          </button>
+          <button
+            onClick={() => setStatus("completed")}
+            className={`text-[var(--color-secondary)] text-lg rounded-lg px-5 py-2 font-semibold ${
+              status == "completed" && "bg-[var(--color-dark-gray)]"
+            }`}
+          >
+            Completed
+          </button>
+        </div>
+      </div>
+
+      <div className="flex flex-wrap gap-6 px-16 py-4">
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+      </div>
+
+      <BottomNav />
+    </section>
+  );
+};
 
 export default Orders;
