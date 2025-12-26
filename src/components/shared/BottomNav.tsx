782bd567a4bb2b0f0c90de2afdab38c4d9343162
Setha Va <vasetha1@gmail.com>
2025-12-26 22:49:45 +0700

    Order modal UI design

--- src/components/shared/BottomNav.tsx
+++ src/components/shared/BottomNav.tsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { BiSolidDish } from "react-icons/bi";
 import { CiCircleMore } from "react-icons/ci";
 import { FaHome } from "react-icons/fa";
 import { MdOutlineReorder, MdTableBar } from "react-icons/md";
 import { useLocation, useNavigate } from "react-router";
+import Modal from "./Modal";
 
 const BottomNav = () => {
   const navigate = useNavigate();
   const { pathname } = useLocation();
 
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
   const isActive = (path: string) => pathname === path;
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
+  const [guestNumber, setGuestNumber] = useState(0);
+  const increase = () => {
+    if (guestNumber >= 6) return;
+    setGuestNumber((prev) => prev + 1);
+  };
+  const decrease = () => {
+    if (guestNumber == 0) return;
+    setGuestNumber((prev) => prev - 1);
+  };
 
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-[var(--color-bg-footer)] p-2 h-16 flex justify-around">
@@ -60,9 +76,65 @@ const BottomNav = () => {
         <p>More</p>
       </button>
 
-      <button className="absolute text-[var(--color-primary)] bg-[#F6B100] rounded-full p-3 items-center bottom-6">
+      <button
+        className="absolute text-[var(--color-primary)] bg-[#F6B100] rounded-full p-3 items-center bottom-6"
+        onClick={openModal}
+      >
         <BiSolidDish />
       </button>
+
+      <Modal title="Create Order" isOpen={isModalOpen} onClose={closeModal}>
+        <div>
+          <label className="block text-[var(--color-secondary)] mb-2 text-sm font-medium">
+            Customer Name
+          </label>
+          <div className="flex items-center rounded-lg p-3 px-4 bg-[var(--color-background)]">
+            <input
+              type="text"
+              name=""
+              placeholder="Enter customer name"
+              id=""
+              className="bg-transparent flex-1 text-white focus:outline-none"
+            ></input>
+          </div>
+        </div>
+        <div className="mt-3">
+          <label className="block text-[var(--color-secondary)] mb-2 text-sm font-medium">
+            Customer Phone
+          </label>
+          <div className="flex items-center rounded-lg p-3 px-4 bg-[var(--color-background)]">
+            <input
+              type="number"
+              name=""
+              placeholder="Enter customer phone"
+              id=""
+              className="bg-transparent flex-1 text-white focus:outline-none appearance-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+            ></input>
+          </div>
+        </div>
+        <div className="mt-3">
+          <label className="block text-[var(--color-secondary)] mb-2 text-sm font-medium">
+            Guest
+          </label>
+          <div className="flex items-center justify-between bg-[var(--color-background)] px-4 py-3 rounded-lg">
+            <button className="text-yellow-500 text-2xl" onClick={decrease}>
+              &minus;
+            </button>
+            <span className="text-white">
+              {guestNumber} {guestNumber > 1 ? "People" : "Person"}
+            </span>
+            <button className="text-yellow-500 text-2xl" onClick={increase}>
+              &#43;
+            </button>
+          </div>
+        </div>
+        <button
+          className="w-full bg-[var(--color-currency)] text-[var(--color-whitesmoke)] rounded-lg py-3 mt-8 hover:bg-yellow-700"
+          onClick={() => navigate("/tables")}
+        >
+          Create Order
+        </button>
+      </Modal>
     </div>
   );
 };
---
+++ src/components/shared/Modal.tsx
@@ -0,0 +1,32 @@
+import React, { type ReactNode } from "react";
+
+interface ModalProps {
+  title: string;
+  isOpen: boolean;
+  onClose: () => void;
+  children: ReactNode;
+}
+
+const Modal = ({ title, onClose, isOpen, children }: ModalProps) => {
+  if (!isOpen) return null;
+  return (
+    <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
+      <div className="bg-[var(--color-card-background)] shadow-lg w-full max-w-lg mx-4 rounded-lg p-4">
+        <div className="flex justify-between items-center px-6 py-4 border-b border-[#333]">
+          <h2 className="text-xl text-[var(--color-whitesmoke)] font-semibold">
+            {title}
+          </h2>
+          <button
+            className="text-gray-500 text-2xl hover:text-gray-300"
+            onClick={onClose}
+          >
+            &times;
+          </button>
+        </div>
+        <div className="p-6">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
